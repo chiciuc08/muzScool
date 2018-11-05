436d03f41b90d4d749613af30b0bd9f4001b3156
Chiciuc Costel <chiciuc_costel2001@yahoo.com>
2018-11-06 00:54:03 +0200

    modificare view noutati layout

--- advanced/frontend/views/noutati/index.php
+++ advanced/frontend/views/noutati/index.php
@@ -9,8 +9,6 @@ $this->title = 'Activitati';
 
 ?>
 <div class="row">
-   
-    
     <div class="col-md-1">
         <?php 
         if (!Yii::$app->user->isGuest&& Yii::$app->user->can('createPost')) {
@@ -18,23 +16,17 @@ $this->title = 'Activitati';
         }
         ?>
     </div>
-<div class="col-md-10"><h2 class=" text-center"> <?= Html::encode($this->title) ?></h2></div>
+    <div class="col-md-10"><h2 class=" text-center"> <?= Html::encode($this->title) ?></h2></div>
     <div class="col-md-1"></div>
        
-        <div class="col-md-12">
-            <?php foreach ($noutati as $articol) {?>
-                
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><h4><?=$articol->noutati_titlu?></h4></div>
-                        <div class="panel-body"><?=$articol->noutati_text?></div>
-                        <div class="panel-footer"><?= Html::a("Detalii", ['view', 'id'=>$articol->noutati_id], ['class' => 'btn btn-info '])?></div>
-                    </div>
-
-                
-
-            <?php } ?>
-
-        </div>
-
+    <div class="col-md-12">
+        <?php foreach ($noutati as $articol) {?>
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4><?=$articol->noutati_titlu?></h4></div>
+                <div class="panel-body"><?=$articol->noutati_text?></div>
+                <div class="panel-footer"><?= Html::a("Detalii", ['view', 'id'=>$articol->noutati_id], ['class' => 'btn btn-info '])?></div>
+            </div>
+        <?php } ?>
     </div>
+</div>
 
--- advanced/frontend/views/noutati/view.php
+++ advanced/frontend/views/noutati/view.php
@@ -7,27 +7,26 @@ use yii\helpers\Html;
 
 $this->title = $model->noutati_titlu;
 ?>
-
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-         <?php 
- if (!Yii::$app->user->isGuest&& Yii::$app->user->can('updatePost', ['post' => $model])) {
-     echo Html::a('Update', ['update', 'id' => $model->noutati_id], ['class' => 'btn btn-primary']);
-     echo Html::a('Delete', ['delete', 'id' => $model->noutati_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]);
- }
- ?>
-    </p>
-
-    <div class="container">
-        
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h4><?=$model->noutati_titlu?></h4></div>
+            <div class="panel-body"><?=$model->noutati_text?></div>
+            <div class="panel-footer"> 
+                <?php 
+                if (!Yii::$app->user->isGuest&& Yii::$app->user->can('updatePost', ['post' => $model])) {
+                    echo Html::a('Update', ['update', 'id' => $model->noutati_id], ['class' => 'btn btn-primary']);
+                    echo Html::a('Delete', ['delete', 'id' => $model->noutati_id], [
+                           'class' => 'btn btn-danger',
+                           'data' => [
+                               'confirm' => 'Are you sure you want to delete this item?',
+                               'method' => 'post',
+                           ],
+                       ]);
+                }
+                ?>
+            </div>
+        </div>
     </div>
-
+</div>
 
